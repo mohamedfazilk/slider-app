890e17039052e93ad0b7d48f04a84adbb158179b
mohamedfazilk <2114921@cognizant.com>
2022-07-19 13:38:44 +0530

    Placing onclick functionality

--- src/App.js
+++ src/App.js
@@ -15,10 +15,22 @@ function App() {
       </h2>
     </div>
     <div className="section-center">
-      { people.map((person, index)=>{
+      { people.map((person, personIndex)=>{
         const {id,image,name,title,quote}= person;
+
+       
+        let position = 'nextSlide';
+        if (personIndex === index) {
+          position = 'activeSlide';
+        }
+        if (
+          personIndex === index - 1 ||
+          (index === 0 && personIndex === people.length - 1)
+        ) {
+          position = 'lastSlide';
+        }
         return ( 
-        <article key={id}>
+        <article className={position} key={id}>
           <img src={image} alt={name} className='person-img'/>
           <h4>{name}</h4>
           <p className="title">{title}</p>
@@ -28,8 +40,8 @@ function App() {
         
         )
       })}
-      <button className='prev'><FiChevronLeft/></button>
-      <button className='next'><FiChevronRight/></button>
+      <button className='prev' onClick={()=>SetIndex(index -1)}><FiChevronLeft/></button>
+      <button className='next' onClick={()=>SetIndex(index +1)}><FiChevronRight/></button>
     </div>
   </section>
   )}
